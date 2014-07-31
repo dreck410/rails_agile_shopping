1369fac997b73a392a1fb9b2deeb583d1548d6ab
Daniel Recker <drecker@proexam.org>
2014-07-31 14:30:26 -0400

    Fixed cart, alternating colors

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  #validates :title, :description, :image_url, :price, presence: true
+  validates :title, :description, :image_url, :price, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true,
@@ -11,17 +11,7 @@ class Product < ActiveRecord::Base
   		with: /.*\.(png|jpg|jpeg|bmp|gif)\z/i,
   		message: 'Must be a gif, png, jpg, jpeg, or bmp image.'
   }
-  validate :both_or_one
 
-  def both_or_one
-    if title.present? and description.present? and image_url.blank?
-       return true
-    elsif title.blank? and description.blank? and image_url.present?
-      return true
-    else
-      errors.add :title, I18n.t("Error")
-    end
-  end
 
   def self.latest
   	Product.order(:updated_at).last
