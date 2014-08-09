b176b4347226821a8586963f965b0859b15a6bf7
Daniel Recker <drecker@proexam.org>
2014-08-08 20:51:57 -0400

    Cart stuff

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully updated.' }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
@@ -56,9 +56,11 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+
+    @line_item.cart.remove_product(@line_item)
+    binding.pry
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to @line_item.cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,6 +73,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id,)
+      params.require(:line_item).permit(:product_id,)
     end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,4 +13,21 @@ class Cart < ActiveRecord::Base
 		return current_item
 	end
 
+	def remove_product(line_item)
+		if line_item.quantity == 1
+			line_item.destroy
+		elsif line_item.quantity > 1
+			line_item.quantity -= 1
+		end
+		#binding.pry
+	end
+
+	def total_price
+		total = 0
+		line_items.each do |item|
+			total += item.total_price
+		end
+		total
+	end
+
 end
